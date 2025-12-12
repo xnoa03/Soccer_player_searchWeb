3252845ca8b8566e942f3665740f5e7918e85e17
ByeongWoo Kim <xnoa03@naver.com>
2025-12-12 16:08:24 +0900

    commit

--- anaproj.py
+++ anaproj.py
@@ -1,8 +1,33 @@
-import  pandas as pd
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, classification_report
+
+#데이터 로드/전처리
+df = pd.read_csv('StudentPerformanceFactors.csv')
+df = df.dropna()
+
+numeric_df = df.select_dtypes(include=['float64', 'int64'])
+plt.figure(figsize=(12, 10))
+sns.heatmap(numeric_df.corr(), annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title('Correlation Heatmap of Student Performance Factors')
+plt.tight_layout()
+plt.savefig('advanced_graph1_heatmap.png') 
+plt.show()
+
+
+df['Pass'] = df['Exam_Score'].apply(lambda x: 1 if x >= 65 else 0)
+
+le = LabelEncoder()
+categorical_cols = df.select_dtypes(include=['object']).columns
+for col in categorical_cols:
+    df[col] = le.fit_transform(df[col])
+
+X = df.drop(['Exam_Score', 'Pass'], axis=1)
+y = df['Pass']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
