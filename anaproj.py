8d0b216c39c86d73681163e206204fd075666373
ByeongWoo Kim <xnoa03@naver.com>
2025-12-12 15:54:08 +0900

    imports

--- anaproj.py
+++ anaproj.py
@@ -1,1 +1,8 @@
 import  pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, classification_report
