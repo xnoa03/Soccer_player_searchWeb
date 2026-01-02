e21d127dc3a6a102a999cbfa2e50d78ce7971f46
xnoa03 <xnoa03@naver.com>
2026-01-02 12:52:06 +0900

    add:selectbar

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import pandas as pd
 from mplsoccer import Radar, FontManager
 import matplotlib.pyplot as plt
 
-st.title('유럽 5대리그 스카우팅 차트 웹')
-
 data=pd.read_csv("players_data-2024_2025.csv")
 data_clear=data.drop_duplicates()
+Players=data_clear['Player']
+
+st.title('유럽 5대리그 스카우팅 차트 웹')
+S_Player = st.selectbox("선수 선택", Players)
+st.write("Selected Player: ",S_Player)
+
+
 print(data_clear[data_clear['Player']=='Florian Wirtz'])
