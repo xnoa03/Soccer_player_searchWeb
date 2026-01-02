3de317c39d7fc4f20651be7d695e4a36838bd3cb
xnoa03 <xnoa03@naver.com>
2026-01-02 13:52:29 +0900

    add:selectbox

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import pandas as pd
 from mplsoccer import Radar, FontManager
 import matplotlib.pyplot as plt
 
-data=pd.read_csv("players_data-2024_2025.csv")
-data_clear=data.drop_duplicates()
-Players=data_clear['Player']
+def main():
+    data=pd.read_csv("players_data-2024_2025.csv")
+    data_clear=data.drop_duplicates()
+    data_clear = data_clear.iloc[:, 1:]
+    Leagues=sorted(data_clear['Comp'].unique())
 
-st.title('유럽 5대리그 스카우팅 차트 웹')
-S_Player = st.selectbox("선수 선택", Players)
-st.write("Selected Player: ",S_Player)
+    st.title('유럽 5대리그 스카우팅 차트 웹')
+    S_League = st.selectbox("리그 선택", Leagues)
+    if(S_League!=None):
+        League_data=data_clear[data_clear['Comp']==S_League]
+        Players=League_data['Player']
+        S_Player = st.selectbox("선수 선택", Players)
+        st.write("Selected Player: ",S_Player)
+        player_data=data_clear[data_clear['Player']==S_Player]
+        st.write(player_data)
 
-
-print(data_clear[data_clear['Player']=='Florian Wirtz'])
+if __name__ == "__main__":
+    main()
